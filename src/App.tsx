53e9032d1c2d2f12722b66243efa1133c570e6b7
Simba-l-l <prokhorovm.v@yanndex.ru>
2022-02-11 19:05:45 +0300

    123

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { connectWalletBeacon, DAppConnection } from './wallet';
+
 
 function App() {
+  const [connection, setConnection] = useState<DAppConnection>()
+  const handleConnectClick = async () => {
+    try {
+      setConnection(await connectWalletBeacon(true, 'mainnet'))
+    } catch(e){
+      alert((e as any).message ?? e);
+    } 
+  } 
+  const disConnectWallet = () => {
+    setConnection(undefined);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={connection ? disConnectWallet : handleConnectClick}>
+        {connection ? connection.pkh : 'Connect Wallet'}
+      </button>
     </div>
   );
 }
